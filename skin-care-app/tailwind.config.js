5ce6626a2afa2d28cf28b25e248aa283072ebdde
takwatouta <124446915+takwatouta@users.noreply.github.com>
2025-04-29 11:22:43 +0100

    Add skin-care-app/tailwind.config.js 1745922161937

---
+++ skin-care-app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./analysis/*.html",
+    "./auth/*.html",
+    "./js/services/*.js",
+    "./css/pages/*.css"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'tajawal': ['Tajawal', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
